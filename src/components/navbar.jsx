c9d0d628e85ed55cd4a78324155eb6697d012a0a
Perizat Ibraeva <perizat.ibraeva.k@gmail.com>
2022-08-24 00:56:48 +0300

    convert class component to functional component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 
-class NavBar extends Component {
-  render() { 
-    return (
-      <nav className="navbar bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">Navbar <span className="badge-pill badge-secondary">{this.props.totalCounters}</span></a>
-        </div>
-      </nav>
-    );
-  }
-}
+// Stateless Functional Component
+const NavBar = (props) => {
+  return (
+    <nav className="navbar bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">Navbar{' '} <span className="badge-pill badge-secondary">
+          {props.totalCounters}</span></a>
+      </div>
+    </nav>
+  );
+};
  
 export default NavBar;
