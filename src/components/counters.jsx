bb8c53c49bb03452f5a92c842253aefa5af3576f
Perizat Ibraeva <perizat.ibraeva.k@gmail.com>
2022-08-23 23:50:08 +0300

    make counter as a controlled component

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,20 +4,47 @@ import Counter from './counter';
 class Counters extends Component {
   state = { 
     counters: [
-      {id: 1, value: 0},
+      {id: 1, value: 4},
       {id: 2, value: 0},
       {id: 3, value: 0},
       {id: 4, value: 0}
     ]
    };
 
+   handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = {...counter};
+    counters[index].value++;
+    this.setState({ counters })
+   };
+
+   handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+   };
+
+   handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters })    
+   };
+
   render() { 
     return (
       <div>
+        <button 
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2">Reset</button>
         {this.state.counters.map(counter => 
-          <Counter key={counter.id} value={counter.value}>
-            <h4>Counter #{counter.id}</h4>
-          </Counter>
+          <Counter 
+            key={counter.id} 
+            onDelete={this.handleDelete} 
+            onIncrement={this.handleIncrement}
+            counter={counter}
+          />
         )}
       </div>
     );
